c80ac1f77dec191f1a9dfffb4d3d273f26ca6546
Sandeep Kolte <sandeep.kolte@gmail.com>
2016-03-21 22:51:02 -0700

    Learning modularity - accept url as an argument

--- learningpython/learningModularity/learningModularity.py
+++ learningpython/learningModularity/learningModularity.py
@@ -7,13 +7,17 @@
 
 # Running from REPL:
 # python3
-# >>> from learningModularity import (fetch_words, print_words)
-# >>> print_words(fetch_words())
+# >>> from learningModularity import (print_words, fetch_words)
+# >>> print_words("This is a list")
 
+# running from command line:
+# python3 learningModularity.py http://sixty-north.com/c/t.txt
+
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-	with urlopen('http://sixty-north.com/c/t.txt') as story:
+def fetch_words(url):
+	with urlopen(url) as story:
 		story_words = []
 		for line in story:
 			line_words = line.decode('utf-8').split()
@@ -21,13 +25,14 @@ def fetch_words():
 				story_words.append(word)
 		return story_words
 
-def print_words(story_words):
-	for word in story_words:
-		print (word)
+def print_items(items):
+	for item in items:
+		print (item)
 
 def main():
-	words = fetch_words()
-	print_words(words)
+	url = sys.argv[1]
+	words = fetch_words(url)
+	print_items(words)
 
 if __name__ == '__main__':
 	main()
