463c61a2513611c607ed261af113b71f3c73c486
Sandeep Kolte <sandeep.kolte@gmail.com>
2016-03-21 22:08:17 -0700

    Learning modularity - added methods

--- learningpython/learningModularity/learningModularity.py
+++ learningpython/learningModularity/learningModularity.py
@@ -1,18 +1,21 @@
 # Learning how to restructure your python code to create modules, functions from a simple routine.
+# this introduces a function so that module can be imported and then the function can be executed on demand.
 # To run on REPL:
 # python3
-# >>> import learningModularity_v1
-# It should print a list of words obtained from 'http://sixty-north.com/c/t.txt'.
+# >>> import learningModularity
+# >>> learningModularity.fetch_words()
+# At this point, it should print a list of words obtained from 'http://sixty-north.com/c/t.txt'.
+# However you can't still run the module from command line.
 
 from urllib.request import urlopen
 
-with urlopen('http://sixty-north.com/c/t.txt') as story:
-	story_words = []
-	for line in story:
-		line_words = line.decode('utf-8').split()
-		for word in line_words:
-			story_words.append(word)
+def fetch_words():
+	with urlopen('http://sixty-north.com/c/t.txt') as story:
+		story_words = []
+		for line in story:
+			line_words = line.decode('utf-8').split()
+			for word in line_words:
+				story_words.append(word)
 
-for word in story_words:
-	print (word)
-
+	for word in story_words:
+		print (word)
