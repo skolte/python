1ac1dc69b2e6d98ec0a150324f6a21f75e701984
Sandeep Kolte <sandeep.kolte@gmail.com>
2016-03-21 22:54:55 -0700

    Learning modularity - Top level functions have two lines between them. Sparse is better than dense.

--- learningpython/learningModularity/learningModularity.py
+++ learningpython/learningModularity/learningModularity.py
@@ -25,9 +25,11 @@ def fetch_words(url):
 				story_words.append(word)
 		return story_words
 
+
 def print_items(items):
 	for item in items:
 		print (item)
+
 
 def main(url):
 	words = fetch_words(url)
